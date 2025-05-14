8f0eb1f203624531089f4d281b28e1b5143ce7a8
Onur <onur.onel2230@gmail.com>
2025-05-13 20:16:28 -0400

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the Frontend</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
